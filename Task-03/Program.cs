ee846b2c62461b59868d33c5b606d47e76e25feb
GromDV <gromdv@gmail.com>
2022-10-15 17:18:07 +0300

    add Task-03

---
+++ Task-03/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0, border = 0;
+
+if (num >= 0)
+{
+    count = -num;
+    border = num;
+}
+else
+{
+    count = num;
+    border = -num;
+}
+
+while (count <= border)
+{
+    if (count == border)
+    {
+        Console.WriteLine($" {count}");
+        count++;
+    }
+    else
+    {
+        Console.Write($" {count},");
+        count++;
+    }
+}
