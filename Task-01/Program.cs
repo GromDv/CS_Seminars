91dbc978bef1753f9c7e006132f778baf9db5446
GromDV <gromdv@gmail.com>
2022-10-15 16:43:23 +0300

    add Task-01

---
+++ Task-01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1*num1 == num2)
+{
+    Console.WriteLine("Num2 is a square jf num1!");
+}
+else 
+{
+    Console.WriteLine("Num2 is NOT a square jf num1!");
+}
