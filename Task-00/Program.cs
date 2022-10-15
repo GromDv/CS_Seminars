f592f7dde5c1b993ea140872ba139d7b16340134
GromDV <gromdv@gmail.com>
2022-10-15 16:32:15 +0300

    add Task-00

--- Task-00/Program.cs
+++ Task-00/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("num2 = ");
+Console.WriteLine($"Квадрат числа {num} = {num * num}");
