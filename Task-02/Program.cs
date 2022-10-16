adec980a509103369357e8c79706ede1b975bdb8
GromDV <gromdv@gmail.com>
2022-10-16 15:40:27 +0300

    Add 2,4,6,8 tasks

---
+++ Task-02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше Числа {num2}");
+}
+else if (num2 > num1)
+{
+    Console.WriteLine($"Число {num1} меньше Числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} равно Числу {num2}");
+}
---
+++ Task-08/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+if (num > 0)
+{
+    while (count <= num)
+    {
+        if (count % 2 == 0)
+        {
+            if ((num - count) > 1)
+            {
+                Console.Write($" {count},");
+            }
+            else
+            {
+                Console.WriteLine($" {count}");
+            }
+        }
+        count++;
+    }
+}
+else
+{
+    Console.WriteLine($"Число {num} не является натуральным!");
+}
