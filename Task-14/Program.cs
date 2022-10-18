d490dc69c9e0831fdd88dfc1b8fb45b4763f0c5b
GromDV <gromdv@gmail.com>
2022-10-18 08:37:03 +0300

    corrected Task-14

--- Task-14/Program.cs
+++ Task-14/Program.cs
@@ -2,8 +2,23 @@
 // вход число и проверяет, кратно ли оно
 // одновременно 7 и 23.
 
+bool CheckDivsn(int par, int div1, int div2)
+{
+    bool res = default;
+
+    if (par % div1 == 0 && par % div2 == 0)
+        res = true;
+    else
+        res = false;
+
+    return res;
+}
+
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num % 7 == 0 && num % 23 == 0) Console.WriteLine($"Число {num} кратно числам 7 и 23");
-else Console.WriteLine($"Число {num} НЕ кратно числам 7 и 23 одновременно");
+if (num % 7 == 0 && num % 23 == 0) Console.WriteLine($"1. Число {num} кратно числам 7 и 23");
+else Console.WriteLine($"1. Число {num} НЕ кратно числам 7 и 23 одновременно");
+
+if (CheckDivsn(num, 7, 23)) Console.WriteLine($"2. Число {num} кратно числам 7 и 23");
+else Console.WriteLine($"2. Число {num} НЕ кратно числам 7 и 23 одновременно");
