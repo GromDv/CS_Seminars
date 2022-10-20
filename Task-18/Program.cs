992a5563cbcbdbdf6411098036be77750b0eb7f9
GromDV <gromdv@gmail.com>
2022-10-20 11:22:23 +0300

    add Task-18

---
+++ Task-18/Program.cs
@@ -0,0 +1,13 @@
+﻿string Quarter(int num)
+{
+    if(num == 1) return "x > 0, y > 0"; 
+    if(num == 2) return "x < 0, y > 0"; 
+    if(num == 3) return "x < 0, y < 0"; 
+    if(num == 4) return "x > 0, y < 0"; 
+    return "Введен неверный номер четверти!";
+}
+
+Console.Write("Введите номер четверти:");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Quarter(quarter));
