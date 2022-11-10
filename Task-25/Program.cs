0ed857ce16cef8ee873f0cbeea930ad4b4e34047
GromDV <gromdv@gmail.com>
2022-11-10 07:55:36 +0300

    Add seminar-4 & dz-4

---
+++ Task-25/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Write("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите натуральное число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if(numB > 0)
+{
+    Console.WriteLine($"Число {numA} в степени {numB} рано {ToDegree(numA,numB)}");
+}
+else
+{
+    Console.WriteLine($"Число {numB} не является натуральным!");
+}
+
+// Метод возводит число par1 в степень par2
+int ToDegree(int par1, int par2)
+{
+    int result = 1;
+    
+    for(int i = 0; i < par2; i++)
+        result *= par1;
+
+    return result;
+}
---
+++ Task-29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+// Метод выводит массив в одну строку на экран
+void PrintArray(int[] param)
+{
+    Console.Write("[");                                 // открываем скобки
+    for (int i = 0; i < param.Length - 1; i++)          
+        Console.Write($"{param[i]}, ");                 // выводим каждый элемент через запятую
+    Console.WriteLine($"{param[param.Length-1]}]");     // отдельно выводим последний элемент и закрываем скобки
+}
+// метод принимает массив и возвращает его заполненным случайными числами от 1 до 99
+int[] FillArray(int[] param)
+{
+    Random rnd = new Random();                          
+    for (int i = 0; i < param.Length; i++)
+        param[i] = rnd.Next(1,99);
+    return param;
+}
+
+int size = 8;                       // задаем размерность массива
+int[] myArray = new int[size];      // создаем массив
+
+PrintArray(FillArray(myArray));     // выводим заполненный массив
