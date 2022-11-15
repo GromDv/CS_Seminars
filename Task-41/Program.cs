87d2dceecdf78938fa819a73ffe48854c5c66c03
GromDV <gromdv@gmail.com>
2022-11-15 13:25:11 +0300

    Add dz-6

--- Task-41/Program.cs
+++ Task-41/Program.cs
@@ -1,5 +1,85 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-
 // 0, 7, 8, -2, -2 -> 2
-
 // 1, -7, 567, 89, 223-> 3
+
+// метод возвращает целочисленный массив, полученный из входной строки par(string), полагая разделителем par2
+int[] ConvertStrToIntArray(string par, char par2)
+{
+    int countWords = 0;
+    for (int i = 0; i < par.Length; i++)
+        if (par[i] == par2) countWords++;   // посчитаем количество разделителей
+    countWords++;                           // добавим 1 для последнего слова
+
+    int[] result = new int[countWords];     // создаем целочисленный массив по вычисленному количеству слов
+    int countWord = 0;
+    string word = "";                       // заготовка для формирования слова
+    for (int i = 0; i < par.Length; i++)
+    {
+        if (par[i] == par2 || i == par.Length - 1)      // если достигнут очередной разделитель, то...
+        {
+            if (i == par.Length - 1)                    // если это последний символ во входной строке, то..
+                word += par[i];                         // добавляем его в слово
+
+            result[countWord] = Convert.ToInt32(word);  // получаем число из слова и пишем в массив
+            countWord++;                                // увеличиваем счетчик слов
+            word = "";                                  // обнуляем заготовку для нового слова
+        }
+        else                                            // если это просто очередной символ, то
+            word += par[i];                             // добавляем его в заготовку
+    }
+    return result;
+}
+// метод возвращает число положительных чисел во входящем массиве
+int CountPositiveNums(int[] par)
+{
+    int res = 0;
+    for (int i = 0; i < par.Length; i++)                                             
+        if (par[i] > 0) res++;
+    return res;
+}
+
+Console.Write("Введите несколько чисел, разделяя пробелами: ");
+string? inputStr = Console.ReadLine();                                                  // ? - учитываем возможность введения пустой строки
+Console.WriteLine();
+
+if (inputStr != null)                                                                   // а было ли хоть что-то введено, не пустая ли срока?
+{
+    int[] inputs = ConvertStrToIntArray(inputStr, ' ');                                 // конвертируем входную строку в числовой массив
+    int numPositives = CountPositiveNums(inputs);                                       // считаем количество положительныч чисел в массиве
+    
+
+    if (numPositives == 11 || numPositives == 12 || numPositives == 13)
+        Console.WriteLine($"Введено {numPositives} положительных чисел!");
+    else if (numPositives % 10 == 1)
+        Console.WriteLine($"Введено {numPositives} число!");
+    else if (numPositives % 10 == 2 || numPositives % 10 == 3 || numPositives % 10 == 4)
+        Console.WriteLine($"Введено {numPositives} положительных числа!");
+    else
+        Console.WriteLine($"Введено {numPositives} положительных чисел!");
+}
+else
+    Console.Write("Числа не были введены!");
+
+/*
+Console.Write("Укажите сколько чисел будет введено: ");
+int numberAll = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[] inputs = new int[numberAll];
+int numPositives = 0;
+for (int i = 0; i < numberAll; i++)
+{
+    Console.Write($"Введите {i}-е число: ");
+    inputs[i] = Convert.ToInt32(Console.ReadLine());
+    if (inputs[i] > 0) numPositives++;
+}
+
+if (numPositives == 11 || numPositives == 12 || numPositives == 13)
+        Console.WriteLine($"Введено {numPositives} положительных чисел!");
+else if (numPositives % 10 == 1)
+    Console.WriteLine($"Введено {numPositives} число!");
+else if (numPositives % 10 == 2 || numPositives % 10 == 3 || numPositives % 10 == 4)
+    Console.WriteLine($"Введено {numPositives} положительных числа!");
+else
+    Console.WriteLine($"Введено {numPositives} положительных чисел!");
+*/
--- Task-43/Program.cs
+++ Task-43/Program.cs
@@ -1,3 +1,26 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите К1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите В1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите К2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите В2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 != k2)
+{
+    double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+    double y = Math.Round(k1 * x + b1, 2);
+
+    Console.WriteLine("Координаты точки пересечения:");
+    Console.WriteLine("x = {0}\ny = {1}", x, y);
+}
+else
+    Console.WriteLine("Прямые параллельны!");
