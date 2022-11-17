611d501b9b889cca90adfa1fadd09e2978074f7e
GromDV <gromdv@gmail.com>
2022-11-17 15:25:44 +0300

    add dz-7

---
+++ Task-50/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max + 1);
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write("\t{0} ", array[i, j]);
+
+        Console.WriteLine("]");
+    }
+    Console.WriteLine();
+}
+// Конвертируем входную строку в целочисленный массив
+int[] ConvertInputStrToIntArray(string input)
+{
+    string[] indexesWords = input.Split(',');
+    int[] result = new int[indexesWords.Length];
+    for (int i = 0; i < indexesWords.Length; i++)
+    {
+        result[i] = Convert.ToInt32(indexesWords[i]);
+    }
+    return result;
+}
+// Метод проверяет индексы
+bool IsIndexCorrect(int[] index) => index[0] >= 0 && index[0] < 3 && index[1] >= 0 && index[1] < 5;
+
+Console.Write("Введите индексы элемента двумерного массива, разделяя запятыми: ");
+string? inputStr = Console.ReadLine();                          // ? - учитываем возможность введения пустой строки
+Console.WriteLine();                                            // Пропускаем строку
+
+if (inputStr != null)                                           // если введена не пустая строка, то...
+{
+    int[] elementIndexes = ConvertInputStrToIntArray(inputStr); // получаем массив введеных индексов 
+    int[,] arr = CreateMatrixRndInt(3, 5, 0, 10);
+    PrintMatrix(arr);
+
+    if (IsIndexCorrect(elementIndexes))
+        Console.WriteLine($"Значение элемента [{elementIndexes[0]}, {elementIndexes[1]}] = {arr[elementIndexes[0], elementIndexes[1]]}");
+    else
+        Console.WriteLine($"Нет такого элемента: [{elementIndexes[0]}, {elementIndexes[1]}]");
+}
+else
+    Console.Write("Числа не были введены!");
---
+++ Task-52/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max + 1);
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write("\t{0} ", array[i, j]);
+
+        Console.WriteLine("]");
+    }
+    Console.WriteLine();
+}
+
+void PrintArrayDbl(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("\t{0:F1} ", array[i]);
+    }
+    Console.WriteLine("]");
+    Console.WriteLine();
+}
+// Метод возвращает массив средних арифметических по столбцам входного двумерн.массива
+double[] CalcAverage(int[,] array)
+{
+    int size = array.GetLength(1);                      // берем кол-во столбцов 
+    double[] average = new double[size];                // создаем выходной массив
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)    // перебираем строки
+        {
+            average[i] += array[j, i];              
+        }
+        average[i] = Math.Round(average[i] / (double)array.GetLength(0), 1);    // делим на кол-во строк
+    }
+    return average;
+}
+
+int[,] mtr = CreateMatrixRndInt(3, 5, 0, 10);
+PrintMatrix(mtr);
+double[] arr = CalcAverage(mtr);
+PrintArrayDbl(arr);
