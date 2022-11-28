6b7d4509fafd4b6d0e78875e361e5cd90c9cffff
GromDv <46959198+GromDv@users.noreply.github.com>
2022-11-28 16:22:36 +0300

    Update Program.cs

--- Task-68 ф-я Аккермана рекурсия/Program.cs
+++ Task-68 ф-я Аккермана рекурсия/Program.cs
@@ -5,15 +5,12 @@
 // Метод возвращает значение функции Аккермана
 int AkkermanFunc(int m, int n)
 {
-    if (m < 0 || n < 0) return 0;
+    if (m < 0 || n < 0) 
+        return 0;
     if (m == 0)
-    {
         return n + 1;
-    }
     if(n == 0)
-    {
         return AkkermanFunc(m - 1, 1);
-    }
     return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
 }
 
